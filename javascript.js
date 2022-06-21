5d796288d0915735f51cc082c1ffa2c1dd592035
heinohen <heinohen@kapsi.fi>
2022-06-21 23:58:24 +0300

    changed from pure js to html+js

--- javascript.js
+++ javascript.js
@@ -40,41 +40,61 @@ function addBookToLibrary(Book) {
 /*fo testing*/
 
 
-const aura = new Book("aura", "hrr", 123, true);
-const fabel = new Book("bauer","me",9,true);
-const theHobbit = new Book("Hobbit", "JRR", 555, false);
+const aura = new Book("aura", "hrr", 123, "xxx");
+const fabel = new Book("bauer","me",9,"xxxxxx");
+const theHobbit = new Book("Hobbit", "JRR", 555, "placeholder");
 
 addBookToLibrary(aura);
 addBookToLibrary(fabel)
 addBookToLibrary(theHobbit);
 
-/* create right side*/
+/* create left side*/
 
-
+/*
 const addbok = document.querySelector('.addbok');
+const formul = document.createElement('form')
+addbok.appendChild(formul);
 
-/*title*/
+
+
+const titleDiv = document.createElement('div');
 const titleField = document.createElement('input');
+const titleLabel = document.createElement('label');
 titleField.type = "text";
 titleField.placeholder = "title"
 titleField.setAttribute('id', 'title');
 titleField.textContent = "title:"
-addbok.appendChild(titleField);
-/*author*/
+titleDiv.appendChild(titleField);
+titleDiv.appendChild(titleLabel);
+formul.appendChild(titleDiv)
+
+
+
+
+const authDiv = document.createElement('div');
+const authLabel = document.createElement('label');
 const authorField = document.createElement('input');
 authorField.type = "text";
 authorField.placeholder = "author"
 authorField.setAttribute('id', 'author');
 authorField.textContent = "author:"
-addbok.appendChild(authorField);
-/*pages*/
+authDiv.appendChild(authLabel);
+authDiv.appendChild(authorField);
+formul.appendChild(authDiv);
+
+
+const pagesDiv = document.createElement('div');
+const pagesLabel = document.createElement('label');
 const pagesField = document.createElement('input');
 pagesField.type = "text";
 pagesField.placeholder = "pages"
 pagesField.setAttribute('id', 'pages');
 pagesField.textContent = "pages:"
-addbok.appendChild(pagesField);
-/*read*/
+pagesDiv.appendChild(pagesLabel);
+pagesDiv.appendChild(pagesField);
+formul.appendChild(pagesDiv);
+
+
 const readField = document.createElement('input');
 readField.type = "text";
 readField.placeholder = "YES / NO"
@@ -82,73 +102,51 @@ readField.setAttribute('id', 'read');
 readField.textContent = "read:"
 addbok.appendChild(readField);
 
+
+
 const subButton = document.createElement('button');
 subButton.textContent = "ADD NEW BOOX";
 subButton.setAttribute('id','sub');
 addbok.appendChild(subButton);
 
+
 const clearLibrary = document.createElement('button');
 clearLibrary.textContent = "CLEAR LIBRARY";
 clearLibrary.setAttribute('id','clearLibrary');
 addbok.appendChild(clearLibrary);
-
+*/
 
 subButton.onclick = () => doThis();
-clearLibrary.onclick = () => removeAllBooks();
+clearLibrary.onclick = () => deleteAll();
 
 
 
 
-
+/* creates new book object and adds it to array*/
 function doThis() {
     const newTitle = document.getElementById('title').value;
     const newAuthor = document.getElementById('author').value;
     const newPages = document.getElementById('pages').value;
-    const newRead = true;   
+    if (newTitle == "" || newAuthor == "" || newPages == "") {
+        console.log('is empty')
+        return;
+    }
 
-    console.log(newTitle);
-    let addThis = new Book(newTitle,newAuthor,newPages,newRead);
+    let isRead = ""
+    const newRead = document.getElementById('read');
+    if (newRead.checked) {
+        isRead = true
+    } else {
+        isRead = false  
+    }
+    let addThis = new Book(newTitle,newAuthor,newPages,isRead);
     addBookToLibrary(addThis);
-    console.log(addThis.info())
-    console.table(myLibrary)
     updateTable();
 }
 
-
-
-
-/*leftside booklist*/
-const booklist = document.querySelector('.booklist');
-booklist.textContent = "hello from the js side"
-
-
-
-
-const tableLength = myLibrary.length;
-console.log(tableLength);
-
-const tableOfBooks = document.createElement('table');
-tableOfBooks.setAttribute('id', 'tableOfBooks');
-tableOfBooks.style.width = "300px";
-const headerRow = document.createElement('tr');
-const titleOfBook = document.createElement('th');
-const authorOfBook = document.createElement('th');
-const pagesOfBook = document.createElement('th');
-const readThisBook = document.createElement('th');
-titleOfBook.textContent = "Title";
-authorOfBook.textContent = "Author";
-pagesOfBook.textContent = "Pages";
-readThisBook.textContent = "Read?";
-
-headerRow.appendChild(titleOfBook);
-headerRow.appendChild(authorOfBook);
-headerRow.appendChild(pagesOfBook);
-headerRow.appendChild(readThisBook);
-tableOfBooks.appendChild(headerRow);
-booklist.appendChild(tableOfBooks);
 populateTable();
 
-/* this one creates the table */
+/* this one populates the table */
 function populateTable() {
     for (let i = 0; i < myLibrary.length; i++) {
         const tr = tableOfBooks.insertRow();
@@ -166,25 +164,27 @@ function populateTable() {
                     td.textContent = myLibrary[i].getPages();
                     break;
                 case 3:
-                    td.textContent = myLibrary[i].getRead();
+                    const status = myLibrary[i].getRead();
+                    if (status == true) {
+
+                    }
                     break;
                 case 4:
                     const thrashButton = document.createElement('input');
                     thrashButton.type = "button";
                     thrashButton.className = "trashButton";
-                    thrashButton.value = "Trash it!";
                     thrashButton.setAttribute('id', 'trash');
                     thrashButton.onclick = () =>  removeOneBook(tr.rowIndex);
                     td.appendChild(thrashButton);
-                    console.log(thrashButton.attributes)
                     break;   
             }
         }
     }
 }
 
+/*update table*/
 function updateTable() {
-    if (tableLength < myLibrary.length) {
+    if (tableOfBooks.rows.length <= myLibrary.length) {
         const tr = tableOfBooks.insertRow();
         for (let j = 0; j < 5; j++) {
             const td = tr.insertCell();
@@ -207,11 +207,20 @@ function updateTable() {
                     thrashButton.className = "trashButton";
                     thrashButton.setAttribute('id', 'trash');
                     thrashButton.onclick = () =>  removeOneBook(tr.rowIndex);
-                    thrashButton.value = "Trash it!";
                     td.appendChild(thrashButton);    
             }
         }
     }
+}
+
+function deleteAll() {
+    let headers = 1;
+
+    while (tableOfBooks.rows.length > headers) {
+        tableOfBooks.deleteRow(tableOfBooks.rows.length -1 );
+    }
+
+    myLibrary = [];
 }
 
 function removeAllBooks() {
@@ -223,10 +232,8 @@ function removeAllBooks() {
 }
 
 function removeOneBook(index) {
-    console.log(index);
     const removeThis = index -1;
-    console.log(removeThis);
     myLibrary.splice(removeThis, 1);
-    removeAllBooks();
-    populateTable();
+    removeAllBooks(); /*just removes all after splicing */
+    populateTable(); /*and adds them to screen after*/
 }
